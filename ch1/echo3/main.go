a3f7dc381572a2e66009cc439356f983b95023da
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2019-08-26 20:37:49 +0100

    ex 3.3

--- ch1/echo3/main.go
+++ ch1/echo3/main.go
@@ -9,12 +9,37 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 //!+
 func main() {
+	start := time.Now()
+
+	var s, sep string
+	for i := 0; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+
+	fmt.Printf("%5.fs elapsed\n", time.Since(start).Nanoseconds())
+
+	s, sep = "", ""
+	for i, arg := range os.Args[1:] {
+		fmt.Println(strconv.Itoa(i) + " " + arg)
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+
+	fmt.Printf("%5.fs elapsed\n", time.Since(start).Nanoseconds())
+
 	fmt.Println(strings.Join(os.Args[1:], " "))
+
+	fmt.Printf("%5.fs elapsed\n", time.Since(start).Nanoseconds())
 }
 
 //!-
